92c8f35a514c250352cb20454c3360bdfe634e85
Pavlo Bilousov <bilousov.p@icloud.com>
2024-01-27 17:27:45 +0100

    commit to trigger the workflow

--- .github/actions/write-good-action/entrypoint.sh
+++ .github/actions/write-good-action/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-sudo add-apt-repository universe
-sudo apt install python3-proselint
+add-apt-repository universe
+apt install python3-proselint
 
 find . -type f -name "*.md" -exec proselint '{}' \;
